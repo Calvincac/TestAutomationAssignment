d7e5dc81edb9d1df35e940db663aa923fb8a7344
Calvin Custodio <calvincac@hotmail.com>
2018-07-23 15:11:27 -0300

    login val removed

--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -10,8 +10,8 @@ Given("an Oradian customer opens the login page") do
 end
 
 When("the user logs in") do
-    login_page.validate_input_fields
-    login_page.enter_incorrect_information
+    #login_page.validate_input_fields
+    #login_page.enter_incorrect_information
     login_page.enter_correct_details
 end
 
